4d85b6bdbdaf9de5d53081762d0aed88f54bcef3
Hammad Afzal <hammadafzal05@gmail.com>
2025-04-17 16:28:34 +0100

    Project Completed

--- pythoncalculator.py
+++ pythoncalculator.py
@@ -1,1 +1,14 @@
-print("python calculator")
+a=int(input('Enter the value:'))
+b=int(input('Enter the value:'))
+x=input('Enter Operation:')
+
+if x=="+":
+    print("result:", a+b)
+elif x=="-":
+    print("result", a-b)
+elif x=="*":
+    print("result:", a*b)
+elif x=="/":
+    print("result", a/b)
+else:
+    print("Invalid Operation")
